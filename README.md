a5166a52526671e082da2e334ba8d000cee0e69b
Robert Lucian Chiriac <robert.lucian.chiriac@gmail.com>
2019-10-02 11:23:52 +0300

    add boiler plate

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # touch-gesture-detection
 A concept library on detecting gestures from an array of touch sensors
+
+## Development
+
+To develop it, clone the repo and run 
+```bash
+virtualenv -p python3 env # make sure you've got python 3.6+ installed
+
+env\Scripts\activate # for Windows-based systems
+source env/bin/activate # for Linux-based systems
+
+pip install --editable .
+```
---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open('requirements.txt') as fp:
+    install_requires = fp.read()
+
+setuptools.setup(
+    name='kiki',
+    version='0.1.0',
+    author='Robert Lucian Chiriac',
+    author_email='robert.lucian.chiriac@gmail.com',
+    description='A concept library for the Kiki AI robot',
+    url='https://github.com/RobertLucian/touch-gesture-detection',
+    install_requires=install_requires,
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Scientific/Engineering'
+    ]
+)
